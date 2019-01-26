e3822ccb8f2336be1c61893724b9049083411a4d
zfmolta <498788698@qq.com>
2019-01-26 22:21:05 +0800

    大发到送

--- webtest/src/com/jj/po/MenuPo.java
+++ webtest/src/com/jj/po/MenuPo.java
@@ -1,4 +1,14 @@
 package com.jj.po;
 
 public class MenuPo {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
